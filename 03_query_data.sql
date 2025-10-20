34d0fac33fba6eb1a475d082076064973d6e401c
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-19 21:23:31 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,18 +1,14 @@
 --Yovana Isabel Palencia Sánchez 174197
+-- Clientes y sus órdenes
+SET enable_hashjoin = OFF;
+EXPLAIN ANALYZE
 SELECT c.name AS cliente, o.id AS orden_id, o.order_date
 FROM customers c
 JOIN orders o ON c.id = o.customer_id;
 
 -- Órdenes y sus productos
+SET enable_hashjoin = OFF;
+EXPLAIN ANALYZE
 SELECT o.id AS orden_id, p.name AS producto, oi.quantity
 FROM orders o
 JOIN order_items oi ON o.id = oi.order_id
-JOIN products p ON oi.product_id = p.id;
-
--- Total gastado por cliente
-SELECT c.name AS cliente, SUM(p.price * oi.quantity) AS total_gastado
-FROM customers c
-JOIN orders o ON c.id = o.customer_id
-JOIN order_items oi ON o.id = oi.order_id
-JOIN products p ON oi.product_id = p.id
-GROUP BY c.name;
