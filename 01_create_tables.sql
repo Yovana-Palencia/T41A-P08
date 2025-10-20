deee7a102b230c0ff7eb3cf293a750a473b0d4e0
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-19 21:35:42 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,37 +1,25 @@
 --Yovana Isabel Palencia Sánchez 174197
+--- Tabla: customers
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    signup_date DATE NOT NULL
-);
-CREATE INDEX idx_customers_city ON customers(city);
-CREATE INDEX idx_customers_signup_date ON customers(signup_date);
-
--- Tabla: products
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price DECIMAL(10,2) NOT NULL
-);
-CREATE INDEX idx_products_name ON products(name);
-
+@@ -20,7 +20,7 @@ CREATE INDEX idx_products_name ON products(name);
 -- Tabla: orders
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     customer_id INT REFERENCES customers(id),
+    customer_id INT NOT NULL REFERENCES customers(id),
     order_date DATE NOT NULL
 );
 CREATE INDEX idx_orders_order_date ON orders(order_date);
-CREATE INDEX idx_orders_customer_id ON orders(customer_id);
-
+@@ -29,8 +29,8 @@ CREATE INDEX idx_orders_customer_id ON orders(customer_id);
 -- Tabla: order_items
 CREATE TABLE order_items (
     id SERIAL PRIMARY KEY,
     order_id INT REFERENCES orders(id),
     product_id INT REFERENCES products(id),
+    order_id INT NOT NULL REFERENCES orders(id),
+    product_id INT NOT NULL REFERENCES products(id),
     quantity INT NOT NULL
 );
 CREATE INDEX idx_order_items_order_id ON order_items(order_id);
-CREATE INDEX idx_order_items_product_id ON order_items(product_id);
